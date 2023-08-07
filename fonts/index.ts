190efac6b859d66ed5dc0f15295b4c320c7deb59
shuvo-me <shuvo.js.dev@gmail.com>
2023-08-07 09:01:49 +0600

    initial commit

---
+++ fonts/index.ts
@@ -0,0 +1,11 @@
+import { Oswald, Urbanist } from "next/font/google";
+
+export const oswald = Oswald({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const urbanist = Urbanist({
+  subsets: ["latin"],
+  display: "swap",
+});
