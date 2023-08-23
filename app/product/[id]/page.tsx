65a7a9b37bf5b70fae2bc1b2ec4e4de324038c22
shuvo-me <shuvo.js.dev@gmail.com>
2023-08-23 07:35:23 +0600

    created product details page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,17 @@
+import { AppContainer } from "@/components";
 import React from "react";
 
 const ProductDetails = ({ params }: { params: { id: string } }) => {
-  return <div>ProductDetails</div>;
+  return (
+    <div className="w-full py-20">
+      <AppContainer>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </AppContainer>
+    </div>
+  );
 };
 
 export default ProductDetails;
