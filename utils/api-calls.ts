987a789a863a8b8b26aa24d1d6277124ea77eb06
shuvo-me <shuvo.js.dev@gmail.com>
2023-10-02 12:45:01 +0600

    added constant file

--- utils/api-calls.ts
+++ utils/api-calls.ts
@@ -1,9 +1,10 @@
 import { ProdcutApiResponseType } from "@/types";
-
-const API_BASE_URL = "http://127.0.0.1:1337/api";
+import { API_BASE_URL } from "./constants";
 export const getProducts = async (): Promise<ProdcutApiResponseType> => {
   const res = await fetch(`${API_BASE_URL}/products?populate=*`);
   const products = await res.json();
   console.log({ products });
   return products;
 };
+
+export const getCategories = async () => {};
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
 import { getProducts } from "./api-calls";
+import { API_BASE_URL } from "./constants";
 
-export { getProducts };
+export { getProducts, API_BASE_URL };
